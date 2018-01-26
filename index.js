2cb3707dcf1bf4ccb746445398bf4375cd2b7158
Adam Bloss <adam.bloss17@gmail.com>
2018-01-26 14:47:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,14 +15,15 @@ function nowServing(line){
   }
 }
 function currentLine(line) {
-return 'The line is currently: '
+return 'The line is currently';
   if (line.length>0){
-   for(var i = 0;i< line.length;i++){
-  line[i] = name
-  return +i+1+'. ' + name+', '
+    return + ': ';
+   for(var i = 0;i<line.length;i++){
+  line[i] = name;
+  return +i+1+'. ' + name+', ';
 }
 }
 else {
-  return +'empty.'
+  return +' empty.';
+}  
 }
-  }
